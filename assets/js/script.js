e303d42f574475d6811e3435865e1405acc44ae6
Ray Badua <rayjonathanbadua@gmail.com>
2023-06-07 14:08:56 -0700

    updated mapbox api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// api keys
 const edamam = {
   appId: `80462c21`,
   appKey: `eac2e3b0f2d31655e1c3859e895185e6`,
@@ -9,7 +10,7 @@ const mapBox = {
 const query = {
   diet: ``,
   healthLabel: `vegetarian`,
-  cuisine: `Italian`,
+  cuisine: `italian`,
   mealTime: ``,
   calories: ``,
   excluded: ``,
@@ -17,10 +18,8 @@ const query = {
   time: ``,
 };
 
-// https://developer.edamam.com/edamam-docs-recipe-api#/ API Reference for edamam
-// https://docs.mapbox.com/playground/search-box/?q=Starbucks&language=en&session_token=0be1d0ab-b318-4ce1-8889-299c9730f4d0 API Reference for mapbox
-
 // Will get recipes based on query variable
+// https://developer.edamam.com/edamam-docs-recipe-api#/ API Reference for edamam
 function requestEdamam() {
   fetch(
     `https://api.edamam.com/api/recipes/v2?type=public&cuisineType=${query.cuisine}&health=${query.healthLabel}&app_id=${edamam.appId}&app_key=${edamam.appKey}`
@@ -34,12 +33,13 @@ function requestEdamam() {
         recipes: [],
         description: `3 recipe results Based on quiz results`,
       };
-      
+
       // Parsed data function, parsed data is stored in results.recipes as an array
       function dataParsed() {
         for (i = 0; i < 3; i++) {
           let recipe = {
             name: data.hits[i].recipe.label,
+            cuisine: data.hits[i].recipe.cuisineType,
             image: data.hits[i].recipe.image,
             url: data.hits[i].recipe.url,
             mealType: data.hits[i].recipe.mealType,
@@ -51,20 +51,56 @@ function requestEdamam() {
       dataParsed();
       console.log(results);
     });
-}
+};
+
+// Will get random recipe from themealdb api
+// https://www.themealdb.com/api.php api for reference
+function randomRecipe() {
+  fetch(`http://themealdb.com/api/json/v1/1/random.php`)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      // console.log(data);
+      let recipe = {
+        name: data.meals[0].strMeal,
+        cuisine: data.meals[0].strArea,
+        image: data.meals[0].strMealThumb,
+        url: data.meals[0].strSource,
+      };
+      console.log(`Random Recipe:`)
+      console.log(recipe);
+    });
+};
 
 // Will make location suggestions based on user's lat & lon
+// https://docs.mapbox.com/playground/search-box/?q=Starbucks&language=en&session_token=0be1d0ab-b318-4ce1-8889-299c9730f4d0 API Reference for mapbox
 function requestMapBox(lat, lon) {
   fetch(
-    `https://api.mapbox.com/search/searchbox/v1/suggest?q=${query.search}&language=en&proximity=${lon},${lat}&session_token=4f1f177e-055c-11ee-a904-2eb5a363657c&access_token=${mapBox.token}`
+    `https://api.mapbox.com/search/searchbox/v1/suggest?q=${query.cuisine}&language=en&proximity=${lon},${lat}&limit=5&poi_category=restaurant,food&session_token=4f1f177e-055c-11ee-a904-2eb5a363657c&access_token=${mapBox.token}`
   )
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      console.log(data);
+      // console.log(data);
+      let results = {
+        locations: [],
+        description: `5 location suggestions from mapbox`,
+      }
+      function dataParsed() {
+        for (i = 0; i < data.suggestions.length; i++) {
+          let location = {
+            name: data.suggestions[i].name,
+            address: data.suggestions[i].full_address,
+          };
+          results.locations.push(location);
+        };
+      };
+      dataParsed();
+      console.log(results);
     });
-}
+};
 
 // Will get location based on IP from ipapi API -> will pass lat & lon to mapbox for suggestions
 function requestLocation() {
@@ -73,11 +109,12 @@ function requestLocation() {
       return response.json();
     })
     .then(function (data) {
-      console.log(data);
-      console.log(`Lat: ${data.latitude}, Lon: ${data.longitude}`);
+      // console.log(data);
+      console.log(`User Location- Lat: ${data.latitude}, Lon: ${data.longitude}`);
       requestMapBox(data.latitude, data.longitude);
     });
-}
+};
 
 requestEdamam();
 requestLocation();
+randomRecipe();
