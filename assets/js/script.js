dcd8137844931502af22410289ef043e83f0c2b2
Ray Badua <rayjonathanbadua@gmail.com>
2023-06-07 11:01:58 -0700

    ipapi api function

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,7 @@ const edamam = {
 const mapBox = {
   token: `pk.eyJ1IjoiN2VsZXZlbm1hbmdvIiwiYSI6ImNsaWtzYjJyYjBpZzMzcXF2ejNhMzRmdHQifQ._6f_pe90uNU8Kf3uDuLbfQ`,
 };
+
 const query = {
   diet: ``,
   healthLabel: ``,
@@ -12,37 +13,51 @@ const query = {
   mealTime: ``,
   calories: ``,
   excluded: ``,
-  search: `chicken`,
+  search: `American`,
   time: ``,
 };
+
 // https://developer.edamam.com/edamam-docs-recipe-api#/ API Reference for edamam
 // https://docs.mapbox.com/playground/search-box/?q=Starbucks&language=en&session_token=0be1d0ab-b318-4ce1-8889-299c9730f4d0 API Reference for mapbox
 
+// Will get recipes based on query variable
 function request() {
   fetch(
     `https://api.edamam.com/api/recipes/v2?type=public&q=${query.search}&cuisine=${query.cuisine}&app_id=${edamam.appId}&app_key=${edamam.appKey}`
   )
-    .then( function (response) {
+    .then(function (response) {
       return response.json();
     })
-    .then( function (data) {
+    .then(function (data) {
       console.log(data);
     });
-};
+}
 
-function requestMapBox() {
+// Will make location suggestions based on user's lat & lon
+function requestMapBox(lat, lon) {
   fetch(
-    `https://api.mapbox.com/search/searchbox/v1/suggest?q=${query.search}&language=en&session_token=0be1d0ab-b318-4ce1-8889-299c9730f4d0&access_token=${mapBox.token}`
+    `https://api.mapbox.com/search/searchbox/v1/suggest?q=${query.search}&language=en&proximity=${lon},${lat}&session_token=4f1f177e-055c-11ee-a904-2eb5a363657c&access_token=${mapBox.token}`
   )
-    .then( function (response) {
+    .then(function (response) {
       return response.json();
     })
-    .then ( function (data) {
+    .then(function (data) {
       console.log(data);
     });
-};
+}
 
-
+// Will get location based on IP from ipapi API -> will pass lat & lon to mapbox for suggestions
+function requestLocation() {
+  fetch(`https://ipapi.co/json/`)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      console.log(`Lat: ${data.latitude}, Lon: ${data.longitude}`);
+      requestMapBox(data.latitude, data.longitude);
+    });
+}
 
 request();
-requestMapBox();
+requestLocation();
