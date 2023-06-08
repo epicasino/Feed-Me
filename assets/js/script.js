cb9140c4f1ea96c38f9ebc6cc1b91210d0a3f18e
mavarreteno <mavarreteno@email.com>
2023-06-07 18:00:43 -0700

    quiz elements added

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,3 +14,65 @@ function request() {
 }
 
 request();
+
+document
+  .getElementById("food-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault(); 
+
+// Store the list of questions
+var questions = document.querySelectorAll('.question');
+var currentQuestionIndex = 0;
+
+// Function to show the current question
+function showCurrentQuestion() {
+  questions.forEach(function(question, index) {
+    if (index === currentQuestionIndex) {
+      question.style.display = 'block';
+    } else {
+      question.style.display = 'none';
+    }
+  });
+}
+
+// Function to handle form submission
+function handleFormSubmit(event) {
+  event.preventDefault(); // Prevent form submission
+
+  // Get user inputs
+  var allergies = [];
+  var checkboxes = document.getElementsByName('allergies');
+  checkboxes.forEach(function(checkbox) {
+    if (checkbox.checked) {
+      allergies.push(checkbox.value);
+    }
+  });
+  var dietRestrictions = document.getElementById('diet').value;
+  var mealTime = document.getElementById('meal-time').value;
+  var prepTime = document.getElementById('prep-time').value;
+  var cuisine = document.getElementById('cuisine').value;
+
+  // Perform decision-making logic
+  var result = "Based on your inputs:<br>";
+  result += "- Allergies: " + allergies.join(', ') + "<br>";
+  result += "- Diet Restrictions: " + dietRestrictions + "<br>";
+  result += "- Meal Time: " + mealTime + "<br>";
+  result += "- Prep Time: " + prepTime + "<br>";
+  result += "- Cuisine: " + cuisine + "<br>";
+  result += "<br>Here's a suggestion: [Your suggestion goes here]";
+
+  // Display the result
+  document.getElementById('result').innerHTML = result;
+
+  // Move to the next question
+  currentQuestionIndex++;
+  if (currentQuestionIndex < questions.length) {
+    showCurrentQuestion();
+  }
+}
+
+// Attach event listener to the form submit
+document.getElementById('food-form').addEventListener('submit', handleFormSubmit);
+
+// Show the first question initially
+showCurrentQuestion();});
