a28d35c4825f12723a131735ccb831398fcabd29
Ray Badua <rayjonathanbadua@gmail.com>
2023-06-08 13:48:03 -0700

    boilerplate function in script

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,6 +18,44 @@ const query = {
   time: ``,
 };
 
+const questionBoxEl = document.querySelector(".question-box");
+const optionBoxesEl = document.querySelector(".option-boxes");
+
+// Array for the form questions for recipe searcher api
+const formQuestionsArray = [
+  {
+    question: `Dietary Preferences?`,
+    options: [`Vegan`, `Keto-Friendly`, `Vegetarian`, `None`],
+  },
+  {
+    question: `Allergies?`,
+    // continue form array to have all questions in issues on github
+  },
+];
+
+function FirstQuestion() {
+  // Makes question text the first question in array
+  questionBoxEl.textContent = formQuestionsArray[0].question;
+  // For loop assigns option array from formQuestionsArray to the four option boxes
+  for (i = 0; i < formQuestionsArray[0].options.length; i++) {
+    //
+    optionBoxesEl.children[i].textContent = formQuestionsArray[0].options[i];
+
+    // WOULDNT RECOMMEND. But if you want to, here's a boilerplate for checkbox.
+
+    // const checkBoxEl = document.createElement('input');
+    // checkBoxEl.setAttribute('type', 'checkbox');
+    // checkBoxEl.setAttribute('name', 'options')
+    // const labelEl = document.createElement('label');
+    // labelEl.setAttribute('for', 'options');
+    // labelEl.textContent = formQuestionsArray[0].options[i];
+    // optionBoxesEl.appendChild(checkBoxEl);
+    // optionBoxesEl.appendChild(labelEl);
+  }
+}
+
+FirstQuestion();
+
 // Will get recipes based on query variable
 // https://developer.edamam.com/edamam-docs-recipe-api#/ API Reference for edamam
 function requestEdamam() {
@@ -182,78 +220,74 @@ requestEdamam();
 requestLocation();
 randomRecipe();
 
-var formEl = document.getElementById("food-form")
+var formEl = document.getElementById("food-form");
 formEl.addEventListener("submit", function (event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    // Store the list of questions
-    var questions = document.querySelectorAll(".question");
-    var currentQuestionIndex = 0;
+  // Store the list of questions
+  var questions = document.querySelectorAll(".question");
+  var currentQuestionIndex = 0;
 
-    // Function to show the current question
-    function showCurrentQuestion() {
-      for (var i = 0; i < questions.length; i++) {
-        var question = questions[i];
-        if (i === currentQuestionIndex) {
-          question.style.display = "block";
-        } else {
-          question.style.display = "none";
-        }
+  // Function to show the current question
+  function showCurrentQuestion() {
+    for (var i = 0; i < questions.length; i++) {
+      var question = questions[i];
+      if (i === currentQuestionIndex) {
+        question.style.display = "block";
+      } else {
+        question.style.display = "none";
       }
     }
-
-    // Function to handle form submission
-    function handleFormSubmit(event) {
-      event.preventDefault(); // Prevent form submission
-
-      // Get user inputs
-      var allergies = [];
-      var checkboxes = document.getElementsByName("allergies");
-      for (var i = 0; i < checkboxes.length; i++) {
-        var checkbox = checkboxes[i];
-        if (checkbox.checked) {
-          allergies.push(checkbox.value);
-        }
-      }
-      var dietRestrictions = document.querySelector(
-        'input[name="restrictions"]:checked'
-      ).value;
-      var mealTime = document.querySelector(
-        'input[name="meal-time"]:checked'
-      ).value;
-      var prepTime = document.querySelector(
-        'input[name="prep-time"]:checked'
-      ).value;
-      var cuisine = document.querySelector(
-        'input[name="cuisine"]:checked'
-      ).value;
-
-      // Perform decision-making logic
-      var result = "Based on your inputs:<br>";
-      result += "- Allergies: " + allergies.join(", ") + "<br>";
-      result += "- Diet Restrictions: " + dietRestrictions + "<br>";
-      result += "- Meal Time: " + mealTime + "<br>";
-      result += "- Prep Time: " + prepTime + "<br>";
-      result += "- Cuisine: " + cuisine + "<br>";
-      result += "<br>Here's a suggestion: [Your suggestion goes here]";
-
-      // Display the result
-      document.getElementById("result").innerHTML = result;
-
-      // Move to the next question
-      currentQuestionIndex++;
-      if (currentQuestionIndex < questions.length) {
-        showCurrentQuestion();
-      }
-    }
-
-    // Attach event listener to the form submit
-    document
-      .getElementById("food-form")
-      .addEventListener("submit", handleFormSubmit);
-
-    // Show the first question initially
-    showCurrentQuestion();
   }
-);
 
+  // Function to handle form submission
+  function handleFormSubmit(event) {
+    event.preventDefault(); // Prevent form submission
+
+    // Get user inputs
+    var allergies = [];
+    var checkboxes = document.getElementsByName("allergies");
+    for (var i = 0; i < checkboxes.length; i++) {
+      var checkbox = checkboxes[i];
+      if (checkbox.checked) {
+        allergies.push(checkbox.value);
+      }
+    }
+    var dietRestrictions = document.querySelector(
+      'input[name="restrictions"]:checked'
+    ).value;
+    var mealTime = document.querySelector(
+      'input[name="meal-time"]:checked'
+    ).value;
+    var prepTime = document.querySelector(
+      'input[name="prep-time"]:checked'
+    ).value;
+    var cuisine = document.querySelector('input[name="cuisine"]:checked').value;
+
+    // Perform decision-making logic
+    var result = "Based on your inputs:<br>";
+    result += "- Allergies: " + allergies.join(", ") + "<br>";
+    result += "- Diet Restrictions: " + dietRestrictions + "<br>";
+    result += "- Meal Time: " + mealTime + "<br>";
+    result += "- Prep Time: " + prepTime + "<br>";
+    result += "- Cuisine: " + cuisine + "<br>";
+    result += "<br>Here's a suggestion: [Your suggestion goes here]";
+
+    // Display the result
+    document.getElementById("result").innerHTML = result;
+
+    // Move to the next question
+    currentQuestionIndex++;
+    if (currentQuestionIndex < questions.length) {
+      showCurrentQuestion();
+    }
+  }
+
+  // Attach event listener to the form submit
+  document
+    .getElementById("food-form")
+    .addEventListener("submit", handleFormSubmit);
+
+  // Show the first question initially
+  showCurrentQuestion();
+});
