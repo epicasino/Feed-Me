b6e02944d5479407393f9024496b35ef5bdcc7c3
mavarreteno <mavarreteno@email.com>
2023-06-08 13:15:46 -0700

    trying hide questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,64 +15,77 @@ function request() {
 
 request();
 
-document
-  .getElementById("food-form")
-  .addEventListener("submit", function (event) {
-    event.preventDefault(); 
+var formEl = document.getElementById("food-form")
+formEl.addEventListener("submit", function (event) {
+    event.preventDefault();
 
-// Store the list of questions
-var questions = document.querySelectorAll('.question');
-var currentQuestionIndex = 0;
+    // Store the list of questions
+    var questions = document.querySelectorAll(".question");
+    var currentQuestionIndex = 0;
 
-// Function to show the current question
-function showCurrentQuestion() {
-  questions.forEach(function(question, index) {
-    if (index === currentQuestionIndex) {
-      question.style.display = 'block';
-    } else {
-      question.style.display = 'none';
+    // Function to show the current question
+    function showCurrentQuestion() {
+      for (var i = 0; i < questions.length; i++) {
+        var question = questions[i];
+        if (i === currentQuestionIndex) {
+          question.style.display = "block";
+        } else {
+          question.style.display = "none";
+        }
+      }
     }
-  });
-}
 
-// Function to handle form submission
-function handleFormSubmit(event) {
-  event.preventDefault(); // Prevent form submission
+    // Function to handle form submission
+    function handleFormSubmit(event) {
+      event.preventDefault(); // Prevent form submission
 
-  // Get user inputs
-  var allergies = [];
-  var checkboxes = document.getElementsByName('allergies');
-  checkboxes.forEach(function(checkbox) {
-    if (checkbox.checked) {
-      allergies.push(checkbox.value);
+      // Get user inputs
+      var allergies = [];
+      var checkboxes = document.getElementsByName("allergies");
+      for (var i = 0; i < checkboxes.length; i++) {
+        var checkbox = checkboxes[i];
+        if (checkbox.checked) {
+          allergies.push(checkbox.value);
+        }
+      }
+      var dietRestrictions = document.querySelector(
+        'input[name="restrictions"]:checked'
+      ).value;
+      var mealTime = document.querySelector(
+        'input[name="meal-time"]:checked'
+      ).value;
+      var prepTime = document.querySelector(
+        'input[name="prep-time"]:checked'
+      ).value;
+      var cuisine = document.querySelector(
+        'input[name="cuisine"]:checked'
+      ).value;
+
+      // Perform decision-making logic
+      var result = "Based on your inputs:<br>";
+      result += "- Allergies: " + allergies.join(", ") + "<br>";
+      result += "- Diet Restrictions: " + dietRestrictions + "<br>";
+      result += "- Meal Time: " + mealTime + "<br>";
+      result += "- Prep Time: " + prepTime + "<br>";
+      result += "- Cuisine: " + cuisine + "<br>";
+      result += "<br>Here's a suggestion: [Your suggestion goes here]";
+
+      // Display the result
+      document.getElementById("result").innerHTML = result;
+
+      // Move to the next question
+      currentQuestionIndex++;
+      if (currentQuestionIndex < questions.length) {
+        showCurrentQuestion();
+      }
     }
-  });
-  var dietRestrictions = document.getElementById('diet').value;
-  var mealTime = document.getElementById('meal-time').value;
-  var prepTime = document.getElementById('prep-time').value;
-  var cuisine = document.getElementById('cuisine').value;
 
-  // Perform decision-making logic
-  var result = "Based on your inputs:<br>";
-  result += "- Allergies: " + allergies.join(', ') + "<br>";
-  result += "- Diet Restrictions: " + dietRestrictions + "<br>";
-  result += "- Meal Time: " + mealTime + "<br>";
-  result += "- Prep Time: " + prepTime + "<br>";
-  result += "- Cuisine: " + cuisine + "<br>";
-  result += "<br>Here's a suggestion: [Your suggestion goes here]";
+    // Attach event listener to the form submit
+    document
+      .getElementById("food-form")
+      .addEventListener("submit", handleFormSubmit);
 
-  // Display the result
-  document.getElementById('result').innerHTML = result;
-
-  // Move to the next question
-  currentQuestionIndex++;
-  if (currentQuestionIndex < questions.length) {
+    // Show the first question initially
     showCurrentQuestion();
   }
-}
-
-// Attach event listener to the form submit
-document.getElementById('food-form').addEventListener('submit', handleFormSubmit);
-
-// Show the first question initially
-showCurrentQuestion();});
+);
